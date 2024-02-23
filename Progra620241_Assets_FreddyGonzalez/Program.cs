bc8a92d8a72d402a26da1da1f1301b39280a4bcf
FREDDY ANDREY GONZALEZ ARTAVIA <andreyga22@gmail.com>
2024-02-22 21:18:28 -0600

    Created connection to bd

--- Progra620241_Assets_FreddyGonzalez/Program.cs
+++ Progra620241_Assets_FreddyGonzalez/Program.cs
@@ -1,22 +1,44 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Progra620241_Assets_FreddyGonzalez.Models;
 
-// Add services to the container.
+internal class Program {
+    private static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        // Add services to the container.
 
-var app = builder.Build();
+        builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        //aca agregamos los valores de conexion a la bd.
+        //1 agregar la info en una etiqueta en el archivo appsettings.json
+        var CnnStrBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("CNNSTR"));
+        //por seguridad es mejor agregar la info de la contrasena en este codigo o podriamos tener un almacen externo. Investigar que son los user Secrets
+        CnnStrBuilder.Password = "progra6";
+
+        //ahora que hemos extraido la cadena de conexion creamos una variable tipo string que la almacene.
+        string CnnStr = CnnStrBuilder.ConnectionString;
+        //ahora conectamos el proyecto a la bd
+        builder.Services.AddDbContext<Progra620241Context>(options => options.UseSqlServer(CnnStr));
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment()) {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
